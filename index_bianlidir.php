65766198826cdff04d3a8636e760c7119184be0c
yi1hu <yi1hu@sina.com>
2017-07-05 11:21:40 +0800

    遍历目录到文件名
    遍历目录到文件名

--- index_bianlidir.php
+++ index_bianlidir.php
@@ -13,23 +13,8 @@ function listDir($dir){
                 listDir($sub_dir);
             }else{
                 echo $sub_dir."<br>";   // 整个文件路径
-//                echo $file."<br>";      // 文件名称
-//                listMess($sub_dir);
             }
         }
     }
 }
-// 获取文件内容，并放入到数组中
-function listMess($sub_dir){
-    $myfile = fopen($sub_dir,"r") or die("fopen fail");
-    $conn = array();
-    $i = 0;   //  循环，把每一行的数据写入到 数组中
-    while (!feof($myfile)) {
-        $conn[$i] = fgets($myfile);
-        $i++;
-    }
-    fclose($myfile);
-    $conn = array_filter($conn);
-    var_dump($conn);
-//    VMess($conn,$sub_dir);
-}
+
